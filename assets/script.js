5bacb43d71296eeff4e46b704677f40457d5a2e5
jmichaeldavis <jmichaeldavis17@gmail.com>
2023-04-10 17:58:57 -0600

    added all the final functionality for javascript

--- assets/script.js
+++ assets/script.js
@@ -2,6 +2,8 @@ var pantryListParentDiv = document.querySelector("#food-list");
 var foodTextInputEl = document.querySelector("#food-text-input");
 var submitFoodBtn = document.querySelector("#submit-food-button");
 var recipeListParentDiv = document.querySelector("#recipe-display");
+var savedRecipesBtn = document.querySelector("#view-saved-recipes-button")
+var savedRecipesKey = "saved_recipes"
 
 // var recipePagePreview = document.querySelector("#recipe");
 
@@ -23,13 +25,19 @@ function addFoodInput() {
   ingredients.push(foodInput.toLowerCase());
   generateURL();
   apiCall();
-  drinkApiCall();
+  // drinkApiCall();
 }
 
 // have a div encompassing current recipes in view with and ID to easily access it. Then when new API call is made, in the .then
 // call a function that accesses that encompassing div and deletes all its children and then rerender them. 
 
+function viewSavedRecipes() {
+  var savedRecipes = JSON.parse(localStorage.getItem(savedRecipesKey)) || []
+  displayRecipies(savedRecipes, true);
+}
+
 submitFoodBtn.addEventListener("click", addFoodInput);
+savedRecipesBtn.addEventListener("click", viewSavedRecipes);
 
 //api call to gather ingredients based on food
 const options = {
@@ -41,17 +49,20 @@ const options = {
 };
 
 // if clicked, store recipe key of specic recipe into variable and then input in this following fetch request
-function getRecipeID() {
-  var recipeID = this[0].id;
-
-  fetch(
+function getRecipeLink(recipeID) {
+ return fetch(
     `https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/${recipeID}/information`,
     options
   )
-    .then((response) => response.json())
-    .then((response) => console.log(response))
+    .then(function (response) {
+      console.log(response);
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data);
+      return data.sourceUrl;
+    })
     .catch((err) => console.error(err));
-  console.log(recipeID);
 }
 
 // recipePagePreview.addEventListener("click", getRecipeID);
@@ -88,15 +99,81 @@ function apiCall() {
     .catch((err) => console.error(err));
 }
 
-function displayRecipies(response) {
+
+function saveRecipe(recipe) {
+  var currentRecipes = JSON.parse(localStorage.getItem(savedRecipesKey)) || []
+  console.log(currentRecipes)
+  if(!currentRecipes.filter(cr => cr.id == recipe.id).length) {
+    currentRecipes.push(recipe)
+  }
+  localStorage.setItem(savedRecipesKey, JSON.stringify(currentRecipes))
+}
+
+async function buildRecipeImage (currentRecipe) {
+  var recipeLinkEl = document.createElement("a");
+  var imageSource = currentRecipe.image;
+  var currentRecipeID = currentRecipe.id
+  var recipeLink = await getRecipeLink(currentRecipeID);
+  recipeLinkEl.innerHTML = `<img src="${imageSource}">`;
+  recipeLinkEl.href = recipeLink;
+  recipeLinkEl.target = "_blank";
+  return recipeLinkEl
+}
+
+function buildViewRecipeButton(recipeLink) {
+  var viewRecipeBtn = document.createElement("a");
+  viewRecipeBtn.href = recipeLink;
+  viewRecipeBtn.target = "_blank";
+  viewRecipeBtn.textContent = "View";
+  return viewRecipeBtn;
+}
+
+function buildSaveRecipeButton(currentRecipe) {
+  var saveRecipeBtn = document.createElement("button");
+  saveRecipeBtn.textContent = "Save";
+  saveRecipeBtn.addEventListener("click", () => saveRecipe(currentRecipe));
+  return saveRecipeBtn
+}
+
+function removeRecipe(recipe) {
+  var currentRecipes = JSON.parse(localStorage.getItem(savedRecipesKey)) || []
+  var newRecipes = currentRecipes.filter(cr => cr.id !== recipe.id)
+  localStorage.setItem(savedRecipesKey, JSON.stringify(newRecipes))
+  displayRecipies(newRecipes, true)
+}
+
+function buildRemoveRecipeButton(currentRecipe) {
+  var removeRecipeBtn = document.createElement("button");
+  removeRecipeBtn.textContent = "Remove";
+  removeRecipeBtn.addEventListener("click", () => removeRecipe(currentRecipe));
+  return removeRecipeBtn
+}
+
+async function displayRecipies(recipes, isSaved = false) {
   refreshRecipies();
-  console.log(response);
-  for (i = 0; i < response.length; i++) {
-    var recipeList = document.createElement("ul");
-    var recipeListEl = document.createElement("li");
-    recipeListEl.textContent = response[i].title;
-    recipeListParentDiv.appendChild(recipeList);
-    recipeList.appendChild(recipeListEl);
+  console.log(recipes);
+  for (i = 0; i < recipes.length; i++) {
+    var currentRecipe = recipes[i]
+    var recipePreview = document.createElement("div");
+    var recipeTitleEl = document.createElement("h3");
+   
+    var recipeImage = await buildRecipeImage(currentRecipe);
+    var viewRecipeBtn = buildViewRecipeButton(recipeImage.href);
+    
+
+   
+    recipeTitleEl.textContent = currentRecipe.title;
+    recipePreview.prepend(recipeImage);
+    recipeListParentDiv.appendChild(recipePreview);
+    recipePreview.appendChild(recipeTitleEl);
+    recipePreview.appendChild(viewRecipeBtn);
+    if(isSaved) {
+      var removeRecipeBtn = buildRemoveRecipeButton(currentRecipe)
+      recipePreview.appendChild(removeRecipeBtn)
+    } else {
+      var saveRecipeBtn = buildSaveRecipeButton(currentRecipe);
+      recipePreview.appendChild(saveRecipeBtn);
+    }
   }
 }
 
@@ -106,17 +183,17 @@ function refreshRecipies() {
 }
 
 //drink API call that returns a random assortment of drinks, they will display all sorts of information on the drink, might need to make a separate display for this.
-function drinkApiCall() {
-  const options = {
-    method: "GET",
-    headers: {
-      "X-RapidAPI-Key": "9e31a75844mshb2d4a2fa7b4e020p1e0a93jsn06a74ba6a801",
-      "X-RapidAPI-Host": "drinks-digital1.p.rapidapi.com",
-    },
-  };
+// function drinkApiCall() {
+//   const options = {
+//     method: "GET",
+//     headers: {
+//       "X-RapidAPI-Key": "9e31a75844mshb2d4a2fa7b4e020p1e0a93jsn06a74ba6a801",
+//       "X-RapidAPI-Host": "drinks-digital1.p.rapidapi.com",
+//     },
+//   };
 
-  fetch("https://drinks-digital1.p.rapidapi.com/v1/cocktails?limit=6", options)
-    .then((response) => response.json())
-    .then((response) => console.log(response))
-    .catch((err) => console.error(err));
-}
+  // fetch("https://drinks-digital1.p.rapidapi.com/v1/cocktails?limit=6", options)
+  //   .then((response) => response.json())
+  //   .then((response) => console.log(response))
+  //   .catch((err) => console.error(err));
+  //}
